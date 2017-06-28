48ffffc07a47fe2e056bad74f8942f2ff8b69bec
WMH <wuliannanWMH@163.com>
2017-06-28 19:55:00 +0800

    初始化静态页面

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,9 +1,41 @@
 <template>
+ <div>
+   <div id="root">
+     <div class="todo-container">
+       <div class="todo-wrap">
+         <demo_header></demo_header>
+         <list></list>
+        <demo_footer></demo_footer>
 
+       </div>
+     </div>
+   </div>
+ </div>
 </template>
 
 <script>
-
+  import footer from "./footer.vue"
+  import header from  "./header.vue"
+  import list from "./list.vue"
+  export default{
+      components : {
+          "demo_header" : header,
+            list,
+          "demo_footer" : footer
+      }
+  }
 </script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/app.vue'
 
 // 创建Vue实例对象
 /*
